64dc3eae11abdc1f0430a9f5feea19e1d655cc99
Dominick Painter <dominick.painter@clock.co.uk>
2019-01-31 13:41:09 +1100

    > Add assets and boilerplate components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from 'react'
+import './App.css'
+import Card from './components/Card'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        Hello
+          <Card />
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
