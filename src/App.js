7105efe55901ccc9e536b850f90097743be88141
Dominick Painter <dominick.painter@clock.co.uk>
2019-01-31 13:57:22 +1100

    > Add and utilise styled-components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component, Fragment } from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 import Card from './components/Card'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: sans-serif;
+  }
+`
+
+const AppContainer = styled.div`
+  text-align: center;
+`
+
+const AppHeader = styled.div`
+  background-color: #d1daec;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: black;
+`
+
+const Title = styled.h1`
+  font-size: 1.5em;
+`
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <Fragment>
+        <AppContainer>
+          <AppHeader>
+            <Title>Show Card</Title>
+          </AppHeader>
           <Card />
-        </header>
-      </div>
+        </AppContainer>
+        <GlobalStyle />
+      </Fragment>
     )
   }
 }
